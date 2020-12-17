7c6d01cad28eaf720ff4c437990bbdb249303861
Huynh Minh Duc <hmduc197@gmail.com>
2020-12-17 18:42:21 +0700

    create layout

---
+++ app/src/main/java/com/hmduc/foody/Constants.kt
@@ -0,0 +1,8 @@
+package com.hmduc.foody
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "c318a5f04f5946419de0e1c354a0e72d"
+    }
+}
---
+++ app/src/main/java/com/hmduc/foody/ui/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.hmduc.foody.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.hmduc.foody.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var navController = findNavController(R.id.navHostFragment)
+        val appBarConfig = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteFragment,
+                R.id.foodJokeFragment
+            )
+        )
+
+        bottomNavBar.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
+}
