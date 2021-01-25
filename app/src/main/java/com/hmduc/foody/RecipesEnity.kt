887bd6eee8015ec2a5a6e1aac2d9623692299553
Huynh Minh Duc <hmduc197@gmail.com>
2021-01-25 19:13:23 +0700

    create enity room

---
+++ app/src/main/java/com/hmduc/foody/RecipesEnity.kt
@@ -0,0 +1,12 @@
+package com.hmduc.foody
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.hmduc.foody.models.FoodRecipe
+import com.hmduc.foody.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEnity(foodRecipe: FoodRecipe) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
